0a3ab9ade364e64e3b84851925fc7a5c7824261b
kharann <anhkhav@gmail.com>
2021-08-29 15:08:35 +0200

    remove unused hello prop

--- frontend/src/components/description.svelte
+++ frontend/src/components/description.svelte
@@ -1,7 +1,3 @@
-<script>
-	export let hello;
-</script>
-
 <div class="mb-10 bg-white p-10 rounded-sm shadow-xl w-full flex justify-center">
 	<p class="max-w-3xl">
 		Linjeforeningen Online ble grunnlagt høsten 1985 på Den allmennvitenskapelige høgskole på Lade.
